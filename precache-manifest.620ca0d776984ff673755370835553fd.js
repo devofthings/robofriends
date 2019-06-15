a58dfdd8948745fb34aa8daa6076b835235c48aa
Chris <saiij@MacBook.local>
2019-06-15 11:42:17 +0200

    Updates

---
+++ precache-manifest.620ca0d776984ff673755370835553fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddfd3f5edfe2f0398bbb1a27310b49d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "588b323345433572bace",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "a26ce899560f1befba29",
+    "url": "/robofriends/static/css/main.4da87c6e.chunk.css"
+  },
+  {
+    "revision": "588b323345433572bace",
+    "url": "/robofriends/static/js/2.590933e2.chunk.js"
+  },
+  {
+    "revision": "a26ce899560f1befba29",
+    "url": "/robofriends/static/js/main.1ce6922a.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
